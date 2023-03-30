b8bac4bc89dad3e14636927b76c17f607f98dc78
Filip <pilif88w@op.pl>
2023-03-30 10:54:25 +0200

    some fixes

--- image_organize.py
+++ image_organize.py
@@ -24,9 +24,15 @@ def create_image_columns(canvas, image_dirs, max_width=200, padding=10):
         images = load_images(path)
         column_frames = []
         for img_name, img in images:
-            img.thumbnail((max_width, max_width), Image.ANTIALIAS)
+            img = resize_and_crop(img, max_width)
             photo = ImageTk.PhotoImage(img)
-            frame = Frame(canvas, bd=2, relief="ridge")
+            frame = Frame(
+                canvas,
+                bd=2,
+                relief="ridge",
+                width=max_width,
+                height=max_width + padding,
+            )
             img_label = Label(frame, image=photo)
             img_label.image = photo
             img_label.pack()
@@ -61,7 +67,7 @@ def get_bottom_position(column_frames):
 
 
 # Handle the release of an image frame during drag-and-drop
-def on_drag_release(event, columns):
+def on_drag_release(event, columns, canvas):
     widget = event.widget
     target_column_index = widget.winfo_x() // 210
     if 0 <= target_column_index < len(columns):
@@ -77,34 +83,58 @@ def on_drag_release(event, columns):
                 break
         if source_column is not None:
             source_column.remove((img_name, widget))
-            
+
+            # Find the source_column_index
+            for idx, column in enumerate(columns):
+                if column == source_column:
+                    source_column_index = idx
+                    break
+
             # Move up images in the source column to close the gap
-            for idx, (_, frame) in enumerate(source_column[source_index:], start=source_index):
-                frame.place(y=210 * idx + idx * 10)
-                
+            for idx, (_, frame) in enumerate(
+                source_column[source_index:], start=source_index
+            ):
+                canvas.create_window(
+                    210 * source_column_index,
+                    (210 * idx + idx * 10) + 50 * (idx + 1),
+                    window=frame,
+                    anchor="nw",
+                )
+
         target_column = columns[target_column_index]
         insert_position = None
-        if not target_column: # empty column
+        if not target_column:  # empty column
             insert_position = 0
-            target_y = 0
+            target_y = 50
         else:
-            for idx, (_, frame) in enumerate(target_column):
-                if widget.winfo_y() < frame.winfo_y():
-                    insert_position = idx
-                    break
-            if insert_position is None:
-                target_y = get_bottom_position(target_column)
-                insert_position = len(target_column)
-            else:
-                target_y = 210 * insert_position + insert_position * 10
-        
+            target_y = (
+                get_bottom_position(target_column) + 50
+            )  # Add 50 to place at the bottom
+            insert_position = len(target_column)
+
         widget.place_forget()
-        widget.place(x=210 * target_column_index, y=target_y)
-        target_column.insert(insert_position if insert_position is not None else len(target_column), (img_name, widget))
-        
+        canvas.create_window(
+            210 * target_column_index, target_y, window=widget, anchor="nw"
+        )
+
+        target_column.insert(
+            insert_position if insert_position is not None else len(target_column),
+            (img_name, widget),
+        )
+
         # Re-adjust the positions of the images below the inserted image in the target column
-        for idx, (_, frame) in enumerate(target_column[insert_position + 1:], start=insert_position + 1):
-            frame.place(y=210 * idx + idx * 10)
+        for idx, (_, frame) in enumerate(
+            target_column[insert_position + 1 :], start=insert_position + 1
+        ):
+            canvas.create_window(
+                210 * target_column_index,
+                (210 * idx + idx * 10) + 50,
+                window=frame,
+                anchor="nw",
+            )
+
+    update_canvas_scrollregion(columns, canvas)
+
 
 # Bind drag-and-drop events to an image frame
 def bind_drag_and_drop(widget, canvas, columns):
@@ -112,12 +142,43 @@ def bind_drag_and_drop(widget, canvas, columns):
     widget.drag_data = {}
     widget.bind("<ButtonPress-1>", on_drag_start)
     widget.bind("<B1-Motion>", on_drag)
-    widget.bind("<ButtonRelease-1>", lambda event: on_drag_release(event, columns))
+    widget.bind(
+        "<ButtonRelease-1>", lambda event: on_drag_release(event, columns, canvas)
+    )
 
 
-# Delete an image frame from the canvas TODO make this move the images and fix bug
-def delete_image(image_frame):
+def resize_and_crop(image, size):
+    width, height = image.size
+    aspect_ratio = float(width) / float(height)
+
+    if aspect_ratio > 1:
+        new_width = size
+        new_height = int(size / aspect_ratio)
+    else:
+        new_width = int(size * aspect_ratio)
+        new_height = size
+
+    image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+    width, height = image.size
+    left = (width - size) / 2
+    top = (height - size) / 2
+    right = (width + size) / 2
+    bottom = (height + size) / 2
+
+    return image.crop((left, top, right, bottom))
+
+
+# Delete an image frame from the canvas TODO make this move the images and fix bug when dragging fter scroll
+def delete_image(image_frame, columns, canvas):
+    for column in columns:
+        for img_name, frame in column:
+            if frame == image_frame:
+                column.remove((img_name, frame))
+                break
+
     image_frame.destroy()
+    update_canvas_scrollregion(columns, canvas)
 
 
 # Enlarge an image in a separate window
@@ -131,9 +192,17 @@ def enlarge_image(image_path):
     img_label.pack()
 
 
-# Main function to
+def update_canvas_scrollregion(columns, canvas):
+    max_y = 0
+    max_x = 210 * len(columns)
+    for column_frames in columns:
+        bottom_y = get_bottom_position(column_frames) + 50
+        if bottom_y > max_y:
+            max_y = bottom_y
+    canvas.configure(scrollregion=(0, 0, max_x, max_y + (210 * len(columns))))
 
 
+# Main function
 def main():
     image_dirs = [
         "images/1",
@@ -144,28 +213,38 @@ def main():
     # Create the main window and set its title and size
     root = Tk()
     root.title("Image Organizer")
-    root.geometry("800x600")
+    root.geometry("800x800")
+    root.resizable(False, False)
+
+    container = Frame(root)
+    container.pack(side="left", fill="both", expand=True)
 
     # Create a canvas and scrollbar for smooth scrolling through the images
-    canvas = Canvas(root, bg="white")
+    canvas = Canvas(container, bg="white", width=800, height=800)
+    scrollbar = Scrollbar(container, orient="vertical", command=canvas.yview)
+    canvas.configure(yscrollcommand=scrollbar.set)
     columns = create_image_columns(canvas, image_dirs)
     canvas.pack(side="left", fill="both", expand=True)
-    scrollbar = Scrollbar(root, orient="vertical", command=canvas.yview)
-    canvas.configure(yscrollcommand=scrollbar.set)
     scrollbar.pack(side="right", fill="y")
+
+    # Create a new frame inside the canvas for placing the images
+    canvas_frame = Frame(canvas)
+    canvas.create_window((0, 0), window=canvas_frame, anchor="nw")
 
     # Draw black lines separating the columns
     for i in range(1, len(columns)):
         canvas.create_line(i * 210, 0, i * 210, 10000, fill="black", width=2)
 
-    # Place the image frames in the canvas and add drag-and-drop and delete/enlarge functionality
+    # Place the image frames in the canvas_frame and add drag-and-drop and delete/enlarge functionality
     for i, column in enumerate(columns):
         for j, (img_name, frame) in enumerate(column):
             bind_drag_and_drop(frame, canvas, columns)
             delete_button = Button(
-                frame, text="X", command=lambda f=frame: delete_image(f)
+                frame,
+                text="X",
+                command=lambda f=frame: delete_image(f, columns, canvas),
             )
-            delete_button.pack(side="top", anchor="e")
+            delete_button.pack(side="left", padx=5)
             enlarge_button = Button(
                 frame,
                 text="Enlarge",
@@ -173,8 +252,12 @@ def main():
                     image_dirs[i], img_name
                 ): enlarge_image(path),
             )
-            enlarge_button.pack(side="top", anchor="w")
-            canvas.create_window(i * 210, j * 210 + j * 10, window=frame, anchor="nw")
+            enlarge_button.pack(side="left", padx=5)
+            canvas.create_window(
+                i * 210, 50 + j * (210 + 50) + j * 10, window=frame, anchor="nw"
+            )
+
+    update_canvas_scrollregion(columns, canvas)
 
     # Resize the canvas when the main window is resized
     def on_resize(event):
